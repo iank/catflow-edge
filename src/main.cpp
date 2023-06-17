dd4ebe7442a40d9df676a1b0406cd5284fe0bc5d
Ian Kilgore <iank@iank.org>
2023-06-17 11:13:56 -0400

    Option to use camera as video source

--- src/main.cpp
+++ src/main.cpp
@@ -18,15 +18,19 @@ int main(int argc, char **argv)
     std::string video_filename = "";
     float confidence_threshold = 0.5;
     bool headless = false;
+    bool camera = false;
 
     app.add_option("model", modelweights_filename, "File containing ONNX model")
         ->required();
     app.add_option("classes", classlist_filename,
                    "A .txt file containing one class name per line")
         ->required();
-    app.add_option("video", video_filename, "Video to run")->required();
+    app.add_option("video", video_filename,
+                   "Video filename or camera index (-W)")
+        ->required();
 
     app.add_flag("-n,--no-gui", headless, "Don't open a GUI window");
+    app.add_flag("-w,--camera", camera, "Video argument is a camera index");
     app.add_option("-c,--confidence", confidence_threshold,
                    "Confidence threshold");
 
@@ -34,12 +38,39 @@ int main(int argc, char **argv)
 
     std::vector<std::string> class_list = load_class_list(classlist_filename);
 
-    cv::Mat frame;
-    cv::VideoCapture capture(video_filename);
+    // Open video file or camera
+    cv::VideoCapture capture;
+    try
+    {
+        if (camera)
+        {
+            int camera_index = std::stoi(video_filename);
+            capture.open(camera_index);
+            capture.set(cv::CAP_PROP_FRAME_WIDTH, 640);
+            capture.set(cv::CAP_PROP_FRAME_HEIGHT, 480);
+        }
+        else
+        {
+            capture.open(video_filename);
+        }
+    }
+    catch (const std::invalid_argument &e)
+    {
+        std::cerr << "Invalid camera index: " << video_filename
+                  << ". The camera index must be an integer." << std::endl;
+        std::exit(EXIT_FAILURE);
+    }
+    catch (const std::out_of_range &e)
+    {
+        std::cerr << "Camera index out of range: " << video_filename
+                  << std::endl;
+        std::exit(EXIT_FAILURE);
+    }
+
     if (!capture.isOpened())
     {
-        std::cerr << "Error opening video file" << std::endl;
-        return -1;
+        std::cerr << "Error opening camera or video file" << std::endl;
+        std::exit(EXIT_FAILURE);
     }
 
     cv::dnn::Net net;
@@ -49,6 +80,7 @@ int main(int argc, char **argv)
     int frame_count = 0;
     float fps = -1;
 
+    cv::Mat frame;
     while (capture.read(frame))
     {
         // Pass frame through model
